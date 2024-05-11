eaa987d44667404676b61b8a60c7477af542d06b
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 18:30:12 +0200

    initial commit

--- src/main/kotlin/Main03.kt
+++ src/main/kotlin/Main03.kt
@@ -1,2 +1,9 @@
-class Main03 {
+fun main() {
+    val firstStudent = Student("Milena","Mrugała")
+    val secondStudent = Student("Monika", "Mrugała", "6")
+    val thirdStudent = Student("Adam", "Mrugała", Grades.DB_PLUS)
+
+    println(firstStudent)
+    println(secondStudent)
+    println(thirdStudent)
 }
