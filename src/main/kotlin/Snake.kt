778fa09adb4ee6ea35f37a20f0ab5c7993b8badc
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 18:30:17 +0200

    initial commit

--- src/main/kotlin/Snake.kt
+++ src/main/kotlin/Snake.kt
@@ -1,2 +1,9 @@
-class Snake {
+class Snake : Animal {
+    override fun speak(): String {
+        return "Ssssss!"
+    }
+
+    override fun eat(): String {
+        return "Warm-blooded prey, insects, fish, amphibians."
+    }
 }
